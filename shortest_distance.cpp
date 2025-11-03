e6e950bd9084b43c98b3e91e59219b32a9dd7e00
Sabari293 <sabarinath7607@gmail.com>
2025-11-03 23:42:06 +0530

    Updated shortest_distance.cpp && added heap.cpp

--- shortest_distance.cpp
+++ shortest_distance.cpp
@@ -1,37 +1,31 @@
 #include "./graph.hpp"
-
-struct cmp{
-    bool operator()(std::pair<Node*, int> a, std::pair<Node*, int> b){
-        return a.second > b.second;
-    }
-};
+#include "./heap.hpp"
 
 void sssp(Graph &G, Node* s, std::map<Node*, bool> &visited){ // Implementation of visited is flexible, kept as map for clarity
-    
-    std::priority_queue<std::pair<Node*, int>, std::vector<std::pair<Node*, int>>, cmp> unknown;
-    std::map<Node*, int> sp; // This can be vector as well, kept as map for clarity
-
+    std::map<Node*,int> val;
+    Heap unknown(val);
+    std::map<Node*, int> sp;// This can be vector as well, kept as map for clarity
     // Initialization...
     for (auto v : visited){
-        if (v.first == s) unknown.push({v.first, 0}); // This won't work unless we change condition or write an operator overload
-        else unknown.push({v.first, __INT32_MAX__});
+        val[v.first]=__INT32_MAX__;
+        unknown.insert(v.first);
     }
     sp[s] = 0;
-
+    unknown.decreasepriority(s);
     // Dijkstra...
-    while (!unknown.empty()){
-        std::pair<Node*, int> v = unknown.top();
-
-        for (Edge* e : G.adj[v.first]){
+    while (unknown.size()>0){
+        auto v = unknown.min();
+        unknown.deleteMin();
+        for (Edge* e : G.adj[v]){
             if (!visited[e->get_dist()]){
                 // Decrease priority of destination Node (idk how, need to think)...
-
-
-                sp[e->get_dist()] = (sp[e->get_dist()] < sp[v.first] + e->get_length()) ? sp[e->get_dist()] : sp[v.first] + e->get_length();
+                val[e->get_dist()]=sp[v] + e->get_length();
+                unknown.decreasepriority(e->get_dist());
+                sp[e->get_dist()] = (sp[e->get_dist()] < sp[v] + e->get_length()) ? sp[e->get_dist()] : sp[v] + e->get_length();
             }
         }
 
-        visited[v.first] = true;
+        visited[v] = true;
     }
 
     return;
