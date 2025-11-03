030946425121e2ac8c37734db0134d96c9cf81ae
Aryaman-Prasad <110331860+Aryaman-Prasad@users.noreply.github.com>
2025-11-03 18:50:59 +0530

    class initializations
    class initializations for Node, Edge, Graph with Dijkstra implementation (partially)

--- graph.hpp
+++ graph.hpp
@@ -1,1 +1,98 @@
+#include <iostream>
+#include <vector>
+#include <map>
+#include <queue>
 
+// Header file for graph, can additionally make something like a graphUtils.cpp or 
+// graphUtils.hpp for implementations any functions defined here
+
+class Node{
+    // Node attributes (id, latitude, longitude etc)...
+
+public:
+    // Constructor...
+    Node(){
+
+    }
+
+    // Destructor...
+    ~Node(){
+
+    }
+
+    // Relevant functions if needed...
+
+};
+
+class Edge{
+    // Edge attributes (id, length, speed_profile, oneway etc)...
+    int length;
+
+    // My rough idea: Edge stores the destination Node, so Graph will be defined
+    // with adjacency list/map with a specific Node having multiple Edges which
+    // inturn consist of the destination Node corresponding to that Edge
+    Node* dest;
+
+public:
+    // Constructor...
+    Edge(){
+
+    }
+
+    // Destructor...
+    ~Edge(){
+
+    }
+
+    // Functions to call for getting certain Edge attributes (can make the variables directly public but this looks cooler hehe)
+    Node* get_dist(){
+        return dest;
+    }
+
+    int get_length(){
+        return length;
+    }
+
+    // Relevant functions if needed...
+
+};
+
+class Graph{
+public:
+    std::map<Node*, std::vector<Edge*>> adj; // Can be vector (hash) instead of map, I have written map for clarity
+    int V; // Number of vertices
+    int E; // Number of esges, if required
+
+    // Constructor...
+    Graph(){
+
+    }
+
+    // Destructor...
+    ~Graph(){
+
+    }
+
+    // Adding a Node
+    void addNode(Node* v){
+
+    }
+
+    // Removing a Node, can implement as complete delete or mark Node to not be used
+    void removeNode(Node* v){
+
+    }
+
+    // Adding an Edge, input source Node and Edge (which consists of destination Node)
+    void addEdge(Node* v, Edge* e){
+
+    }
+
+    // Removing an Edge, implementation for this too is flexible, here Node (source) has to be taken as input (or maybe not necessary?)
+    void removeEdge(Node* v, Edge* e){
+
+    }
+
+    // Other relevant functions, whatever required...
+
+};
--- shortest_distance.cpp
+++ shortest_distance.cpp
@@ -1,1 +1,38 @@
+#include "./graph.hpp"
 
+struct cmp{
+    bool operator()(std::pair<Node*, int> a, std::pair<Node*, int> b){
+        return a.second > b.second;
+    }
+};
+
+void sssp(Graph &G, Node* s, std::map<Node*, bool> &visited){ // Implementation of visited is flexible, kept as map for clarity
+    struct cmp;
+    std::priority_queue<std::pair<Node*, int>, std::vector<std::pair<Node*, int>>, cmp> unknown;
+    std::map<Node*, int> sp; // This can be vector as well, kept as map for clarity
+
+    // Initialization...
+    for (auto v : visited){
+        if (v.first == s) unknown.push({v.first, 0}); // This won't work unless we change condition or write an operator overload
+        else unknown.push({v.first, INT32_MAX});
+    }
+    sp[s] = 0;
+
+    // Dijkstra...
+    while (!unknown.empty()){
+        std::pair<Node*, int> v = unknown.top();
+
+        for (Edge* e : G.adj[v.first]){
+            if (!visited[e->get_dist()]){
+                // Decrease priority of destination Node (idk how, need to think)...
+
+
+                sp[e->get_dist()] = (sp[e->get_dist()] < sp[v.first] + e->get_length()) ? sp[e->get_dist()] : sp[v.first] + e->get_length();
+            }
+        }
+
+        visited[v.first] = true;
+    }
+
+    return;
+}
