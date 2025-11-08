2a402e19245f3c8887675ecdfbecc35d170b9973
Sabari293 <sabarinath7607@gmail.com>
2025-11-08 19:57:52 +0530

    Refactor Dijkstra's algorithm implementation

--- Phase-1/shortest_distance.cpp
+++ Phase-1/shortest_distance.cpp
@@ -8,7 +8,7 @@
 
 
 void sssp(Graph &G, Node* s, std::vector<double> &sp, std::vector<int> &parent, std::map<std::string, bool> &forbidden_roads){ // Implementation of sp and parent is flexible, kept as unordered_map for clarity
-    std::priority_queue<std::pair<Node*, double>, std::vector<std::pair<Node*, double>>, cmp> unknown;
+    std::priority_queue<std::pair<double, Node*>, std::vector<std::pair<double, Node*>>, std::greater<>> unknown;
 
     // If source node is restricted then even god does not know what to do...
     if (s->isRestricted()){
@@ -23,31 +23,31 @@ void sssp(Graph &G, Node* s, std::vector<double> &sp, std::vector<int> &parent, 
     // Now they are vectors :D
 
     sp[s->getid()] = 0.0;
-    unknown.push({s, 0.0});
+    unknown.push({0.0,s});
 
     // Dijkstra...
     while (!unknown.empty()){
-        std::pair<Node*, double> v = unknown.top();
+        std::pair<double, Node*> v = unknown.top();
         unknown.pop();
 
-        if (v.second != sp[v.first->getid()]){
+        if (v.first != sp[v.second->getid()]){
             continue;
         }
 
-        for (Edge* e : G.adj[v.first->getid()]){ // Noe it will work :D
+        for (Edge* e : G.adj[v.second->getid()]){ // Noe it will work :D
             if (e->get_dest()->isRestricted() || e->isRestricted() || forbidden_roads[e->getType()]){
                 continue; // Skip if either the Edge or the destination Node is restricted...
             }
             
-            if (sp[v.first->getid()] + e->get_length() < sp[e->get_dest()->getid()]){
+            if (sp[v.second->getid()] + e->get_length() < sp[e->get_dest()->getid()]){
                 // Updating shortest path
-                sp[e->get_dest()->getid()] = sp[v.first->getid()] + e->get_length();
+                sp[e->get_dest()->getid()] = sp[v.second->getid()] + e->get_length();
 
                 // Updating parent, may be required for other purposes
-                parent[e->get_dest()->getid()] = v.first->getid();
+                parent[e->get_dest()->getid()] = v.second->getid();
 
                 // Adding updated element into heap, deleting previous version of it is not necessary (or is it??)
-                unknown.push({e->get_dest(), sp[e->get_dest()->getid()]});
+                unknown.push({ sp[e->get_dest()->getid()],e->get_dest()});
             }
         }
     }
