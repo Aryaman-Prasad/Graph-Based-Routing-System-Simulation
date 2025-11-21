3adc59cbe2bb67439ac553b5e5da48906038622d
Shr1ram741 <shriram9625@gmail.com>
2025-11-21 21:21:09 +0530

    Perturb() and getInitial() left

--- Phase-3/vrp.cpp
+++ Phase-3/vrp.cpp
@@ -1,36 +1,10 @@
 #include "graph.hpp"
+#include "p_path.hpp"
+#include "ls.cpp"
 
 #ifndef INF
 #define INF 1000000000
 #endif
-
-class Solution {
-    std::vector<std::vector<Edge*>> routes;
-public:
-    double getCost() {
-        //TODO: Devise a cost function for each solution
-    }
-};
-
-class LocalSearch {
-public:
-    LocalSearch(){}
-
-    void IntraRouteSwap(Solution& s);
-    void IntraRoute2Opt(Solution& s);
-    void IntraRouteReinsertion(Solution& s);
-
-    void InterRouteSwap(Solution& s);
-    void InterRouteReinsertion(Solution& s);
-
-private:
-    bool applySwap(Solution& s);          // returns true if solution improved
-    bool apply2Opt(Solution& s);
-    bool applyReinsertion(Solution& s);
-
-    // helpers
-    void updateCost(Solution& s);
-};
 
 Solution getInitialSolution(Graph& G, int n, std::map<int,int> checkpoints) {
     Solution s;
@@ -38,10 +12,27 @@ Solution getInitialSolution(Graph& G, int n, std::map<int,int> checkpoints) {
     return s;
 }
 
-Solution RVND(Solution& s) {
-    Solution best;
-    //TODO: Get the best solution among all neighbourhoods
-    return best;
+void RVND(Solution& s, Graph& G) {
+    std::vector<int> neighborhoods = {1,2,3,4,5}; // Swap, Reinsertion, 2-opt
+    bool improved = true;
+
+    while (improved) {
+        std::shuffle(neighborhoods.begin(), neighborhoods.end(), std::mt19937{std::random_device{}()});
+        improved = false;
+        for (int nh : neighborhoods) {
+            bool ok = false;
+            if (nh == 1) {ok = IntraRouteSwap(s, G);}
+            else if (nh == 2) {ok = IntraRouteReinsertion(s, G);}
+            else if (nh == 3) {ok = IntraRoute2Opt(s, G);}
+            else if (nh==4) {ok = InterRouteSwap(s, G);}
+            else if (nh==5) {ok = IntraRouteReinsertion(s, G);}
+
+            if (ok) {
+                improved = true;
+                break;
+            }
+        }
+    }
 }
 
 void perturb(Graph& G, Solution& s, int n) {
@@ -51,13 +42,13 @@ void perturb(Graph& G, Solution& s, int n) {
 //Don't transfer Solution objects around, maybe use pointers instead
 Solution vrpspd(Graph& G, int n, int max_iter, std::map<int,int> checkpoints) {
     Solution best = getInitialSolution(G, n, checkpoints);
-    //RVND(best);
+    RVND(best,G);
 
     for (int iter = 0; iter < max_iter; iter++) {
         Solution s = best;
         perturb(G,s,n);
-        //RVND(s);
-        if (s.getCost() < best.getCost()) {
+        RVND(s,G);
+        if (s.getCost(G) < best.getCost(G)) {
             best = s;
             iter = 0;
         }
